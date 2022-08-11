24584b2657901459cd71d3654ca5edd60463880e
Patrick <patrickducusin2@gmail.com>
2022-08-11 11:53:28 -0400

    lowercase footer_banner_container styling

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -27,7 +27,7 @@ const FooterBanner = ({ footerBanner: {
   */
   
   return (
-    <div className={styles.Footer_Banner_Container}>
+    <div className={styles.footer_banner_container}>
        <div className={styles.left}>
          <p>{discount}</p>
          <h3>{largeText1}</h3>
