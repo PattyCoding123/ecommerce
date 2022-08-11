4458f205b3d9ade1d797de73bb40509d17d65fc7
Patrick <patrickducusin2@gmail.com>
2022-08-10 21:32:44 -0400

    Removed banner-desc div from FooterBanner, change scss stylings for FooterBanner.

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -27,28 +27,26 @@ const FooterBanner = ({ footerBanner: {
   
   return (
     <div className="footer-banner-container">
-      <div className="banner-description">
-        <div className="left">
-          <p>{discount}</p>
-          <h3>{largeText1}</h3>
-          <h3>{largeText2}</h3>
-          <p>{saleTime}</p>
-        </div>
+       <div className="left">
+         <p>{discount}</p>
+         <h3>{largeText1}</h3>
+         <h3>{largeText2}</h3>
+         <p>{saleTime}</p>
+       </div>
 
-        <div className="right">
-          <p>{smallText}</p>
-          <h3>{midText}</h3>
-          <p>{desc}</p>
-          <Link href={`/product/${product}`}>
-            <button type="button">{buttonText}</button>
-          </Link>
-        </div>
+       <div className="right">
+         <p>{smallText}</p>
+         <h3>{midText}</h3>
+         <p>{desc}</p>
+         <Link href={`/product/${product}`}>
+           <button type="button">{buttonText}</button>
+         </Link>
+       </div>
 
-        <img 
-          src={urlFor(image)}
-          className="footer-banner-image" alt="banner-product"
-        />
-      </div>
+       <img 
+         src={urlFor(image)}
+         className="footer-banner-image" alt="banner-product"
+       />
     </div>
   )
 }
